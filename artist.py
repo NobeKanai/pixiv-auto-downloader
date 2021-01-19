675ce61515c143f9237b103aa9c88d0efe86950e
NobeKanai <qidada4880@gmail.com>
2021-01-19 11:51:44 +0800

    OPT: exists check

--- artist.py
+++ artist.py
@@ -1,10 +1,9 @@
 import logging
+import os
 from typing import List
 from pixivapi.client import Client
 from pixivapi.enums import Size
 from pathlib import Path
-
-from pixivapi.models import Illustration
 
 
 class Artist:
@@ -28,13 +27,6 @@ class Artist:
         """ download all pics which never exists in basepath """
         pass
 
-    def _check_exists(self, illust: Illustration, directory: Path) -> bool:
-        for p in directory.iterdir():
-            if p.match(str(illust.id) + '*'):
-                return True
-
-        return False
-
     def download(self) -> List[Path]:
         """ download start fetch recently updated arts from artist.
         for first time running, it will fetch all pics and compare to local folder to find the difference
@@ -48,23 +40,25 @@ class Artist:
         directory = Path(self.basedir) / self.subdir
         Path.mkdir(directory, parents=True, exist_ok=True)
 
-        paths = []
+        # init pic list if not
+        if len(self.pic_list) == 0:
+            self._init_pic_list(directory)
+
+        paths = []  # for return
 
         response = self.client.fetch_user_illustrations(self.artist_id)
         for illust in response['illustrations']:
             if illust.id in self.pic_list:
+                logging.info("Artist {} ID {} skipped".format(
+                    self.subdir, illust.id))
                 continue
 
-            if not self._check_exists(illust, directory):
-                illust.download(directory=directory, size=Size.ORIGINAL)
-                # add to path
-                paths.append([
-                    i for i in directory.iterdir()
-                    if i.match(str(illust.id) + '*')
-                ][0])
-            else:
-                logging.info(
-                    f"[{self.subdir}]: art <ID:{illust.id}> exists, jumped!")
+            illust.download(directory=directory, size=Size.ORIGINAL)
+            # add to path
+            paths.append([
+                i for i in directory.iterdir()
+                if i.match(str(illust.id) + '*')
+            ][0])
 
             # update pic list
             self.pic_list.append(illust.id)
@@ -72,3 +66,14 @@ class Artist:
                 self.pic_list = self.pic_list[length - 30:]
 
         return paths
+
+    def _init_pic_list(self, dirpath: Path):
+        paths = sorted(dirpath.iterdir(), key=os.path.getmtime, reverse=True)
+        if len(paths) > 30:
+            paths = paths[:30]
+
+        for p in paths:
+            if p.is_file():
+                self.pic_list.insert(0, int(p.name.split('.')[0]))
+            else:
+                self.pic_list.insert(0, int(p.name))
